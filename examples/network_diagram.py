eb6fb6c9537ffa2be22c1179f25eb900a48e09cb
Justin Jeffery <justin.jeffery@ipfabric.io>
2022-02-11 15:26:44 -0500

    docs: updated examples

--- examples/network_diagram.py
+++ examples/network_diagram.py
@@ -1,26 +1,26 @@
 """
 network_diagram.py
 """
-from ipfabric_diagrams import IPFDiagram, Network, NetworkSettings, VALID_NET_PROTOCOLS
+from ipfabric_diagrams import IPFDiagram, Network, NetworkSettings, VALID_NET_PROTOCOLS, Layout
 
 
 if __name__ == '__main__':
     ipf = IPFDiagram()
 
-    # net = Network(sites=['MPLS', 'LAB01'], all_network=True)
-    # json_data = ipf.diagram_json(net)
-    # model_data = ipf.diagram_model(net)
-    #
-    # settings = NetworkSettings()
-    # settings.hide_protocol('xdp')
-    # png_data = ipf.diagram_png(net, graph_settings=settings)
-    # with open('network.png', 'wb') as f:
-    #     f.write(png_data)
-    #
-    # settings.ungroup_group('Layer 3')
-    # svg_data = ipf.diagram_svg(net, graph_settings=settings)
-    # with open('network.svg', 'wb') as f:
-    #     f.write(svg_data)
+    net = Network(sites=['MPLS', 'LAB01'], all_network=True)
+    json_data = ipf.diagram_json(net)
+    model_data = ipf.diagram_model(net)
+
+    settings = NetworkSettings()
+    settings.hide_protocol('xdp')
+    png_data = ipf.diagram_png(net, graph_settings=settings)
+    with open('network.png', 'wb') as f:
+        f.write(png_data)
+
+    settings.ungroup_group('Layer 3')
+    svg_data = ipf.diagram_svg(net, graph_settings=settings)
+    with open('network.svg', 'wb') as f:
+        f.write(svg_data)
 
     settings = NetworkSettings()
     for proto in VALID_NET_PROTOCOLS:
@@ -31,8 +31,9 @@ if __name__ == '__main__':
     settings.hide_protocol('ldp')
     settings.hide_protocol('ebgp')
     settings.change_label('ospf', 'subnet')
-    graph_result = ipf.diagram_model(Network(sites='MPLS'), graph_settings=settings)
-    svg_data = ipf.diagram_svg(Network(sites='MPLS'), graph_settings=settings)
+    mpls = Network(sites='MPLS', layouts=[Layout(path='MPLS', layout='radial')])
+    graph_result = ipf.diagram_model(mpls, graph_settings=settings)
+    svg_data = ipf.diagram_svg(mpls, graph_settings=settings)
     with open('network_edited.svg', 'wb') as f:
         f.write(svg_data)
 
