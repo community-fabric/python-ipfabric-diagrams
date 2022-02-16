5887d1557a840e32e458869f215f0d6d9376e726
Justin Jeffery <justin.jeffery@ipfabric.io>
2022-02-16 14:05:58 -0500

    fix: functional tests

--- tests/functional/test_graphs.py
+++ tests/functional/test_graphs.py
@@ -4,39 +4,39 @@ import unittest
 from ipfabric import IPFClient
 from pkg_resources import parse_version
 
+from ipfabric_diagrams import IPFDiagram, Network, Host2GW
+
 condition = False if os.getenv('IPF_TOKEN', None) and os.getenv('IPF_URL', None) else True
 
 
 @unittest.skipIf(condition, "IPF_URL and IPF_TOKEN not set")
 class MyTestCase(unittest.TestCase):
     def test_site(self):
-        ipf = IPFClient()
+        ipf = IPFDiagram()
         if parse_version(ipf.os_version) < parse_version("4.3"):
             self.skipTest('IP Fabric version under 4.3')
-        graph = ipf.graphs.site('$main')
+        graph = ipf.diagram_json(Network())
         self.assertIsInstance(graph, dict)
 
     def test_picture(self):
-        ipf = IPFClient()
+        ipf = IPFDiagram()
         if parse_version(ipf.os_version) < parse_version("4.3"):
             self.skipTest('IP Fabric version under 4.3')
-        ipf.graphs.style = 'png'
-        graph = ipf.graphs.site('$main')
+        graph = ipf.diagram_png(Network())
         self.assertIsInstance(graph, bytes)
-        ipf.graphs.style = 'svg'
-        graph = ipf.graphs.site('$main')
+        graph = ipf.diagram_svg(Network())
         self.assertIsInstance(graph, bytes)
 
-    def test_h2gw(self):
+    def test_bad_token(self):
         with self.assertRaises(ConnectionRefusedError) as err:
-            IPFClient(token='BAD')
+            IPFDiagram(token='BAD')
 
-    def test_inventory(self):
-        ipf = IPFClient()
+    def test_host2gw(self):
+        ipf = IPFDiagram()
         if parse_version(ipf.os_version) < parse_version("4.3"):
             self.skipTest('IP Fabric version under 4.3')
-        host = ipf.fetch('tables/addressing/hosts', columns=["ip"], limit=1)[0]['ip']
-        graph = ipf.graphs.host_to_gw(host)
+        host = IPFClient().fetch('tables/addressing/hosts', columns=["ip"], limit=1)[0]['ip']
+        graph = ipf.diagram_json(Host2GW(startingPoint=host))
         self.assertIsInstance(graph, dict)
 
 
